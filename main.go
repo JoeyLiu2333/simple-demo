80bcce2ae5c4b7fa362081e8d8fdbea06c46c5bd
joey <joeyliu99@163.com>
2022-05-06 15:48:04 +0800

    first commit

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ func main() {
 	r := gin.Default()
 
 	initRouter(r)
+	// undefined initRouter: configuration中选择package,填入github路径
+	// 或 go run main.go router.go
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
